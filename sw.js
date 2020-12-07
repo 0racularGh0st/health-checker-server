c19efc78ec4972a2c6eaf0bd3e0a565f64415f87
0racularGh0st <nigel4tariang@gmail.com>
2020-12-07 21:30:11 +0530

    update sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
+      body: e.data? e.data.text():"",
       icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
